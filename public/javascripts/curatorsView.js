762c4288b3661829baf21b7bd0267cf4b90957b3
Kang Liu <kgliu0101@gmail.com>
2015-05-07 15:38:27 -0700

    added these 3 files

---
+++ public/javascripts/curatorsView.js
@@ -0,0 +1,7 @@
+// Jquery
+$(document).ready(function() {
+	$("#curators_link").css("text-decoration", "none")
+	$("#userinfo").css('display', 'block')
+	$("#toplinks").css('display', 'none')
+	$("#pairbox").css('display','block')
+});
---
+++ public/javascripts/spindleView.js
@@ -0,0 +1,8 @@
+// Jquery
+$(document).ready(function() {
+	$("#summarydataboard").css('display','none');
+	$("#userinfo").css('display', 'block');
+	$("#toplinks").css('display','none');
+	$("#pwdwin").css('display','none');
+	$("#pairbox").css('display', 'block');
+});
